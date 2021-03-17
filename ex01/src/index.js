ab9f161c834b07bc350efe5df34f9336046215a5
hajdukka <nerminhajdo@gmail.com>
2021-03-17 21:57:01 +0100

    dodao ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,11 @@
+var myPet = {
+    species: "Malinoa",
+    name: "Bethoven",
+    legs: 4,
+    friends: ['Barista', 'Da Vicni']
+}
+function myFunction(obj)
+return obj
+
+console.log(myFunction(obj));
+module.exports = {myPet, myFunction};
